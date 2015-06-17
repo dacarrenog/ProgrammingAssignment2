8c0738f4328c4bd0ac5f5e4b08aa89db29b05f45
Diego <dacarrenog@gmail.com>
2015-06-17 20:51:32 +0100

    Assignment Submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The function makeCacheMatrix is used as the cache matrix, containing the
+## inverse of a given matrix. Then cacheSolve uses the results of
+## makeCacheMatrix to get the inverse of a given matrix. If the inverse has not
+## been calculated, then it calculates it for the first time, but if it has
+## been calculated previously then it simply returns the cached version.
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a special list of functions:
+##1. set the value of the matrix
+##2. get the value of the matrix
+##3. set the value of the inverse
+##4. get the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        s <- NULL
+        set <- function(y) {
+                x <<- y
+                s <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) s <<- solve
+        getsolve <- function() s
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve calculates the inverse of the special "matrix" created with the
+## above function. However, it first checks to see if the inverse has already
+## been calculated. If so, it gets the inverse from the cache and skips the
+## computation. Otherwise, it calculates the inverse of the data and sets the
+## inverse matrix in the cache via the setsolve function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        s <- x$getsolve()
+        if(!is.null(s)) {
+                message("getting cached data")
+                return(s)
+        }
+        data <- x$get()
+        s <- solve(data, ...)
+        x$setsolve(s)
+        s ## Return a matrix that is the inverse of 'x'
 }
